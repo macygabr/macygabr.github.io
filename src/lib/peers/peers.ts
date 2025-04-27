ddc6f2c845df88038a3c04e2dd402abfb759a449
macygabr <macygabr@student.21-school.ru>
2025-04-27 15:56:43 +0700

    fix

--- src/lib/peers/peers.ts
+++ src/lib/peers/peers.ts
@@ -13,11 +13,12 @@ export const getPeers = async (page: number, size: number, campusId: string) => 
     try {
         const response = await apiClient.get('/statistics/peers', {
             params: {
-                page,
-                size,
-                campusId,
+                page: page || 0, // Если значение page пустое, передаем 0 по умолчанию
+                size: size || 25, // Если значение size пустое, передаем 25 по умолчанию
+                campusId: campusId || '46e7d965-21e9-4936-bea9-f5ea0d1fddf2', // Передаем campusId
             },
         });
+
         return response.data; // Возвращаем данные, пришедшие с сервера
     } catch (error) {
         console.error('Ошибка загрузки пользователей:', error);
