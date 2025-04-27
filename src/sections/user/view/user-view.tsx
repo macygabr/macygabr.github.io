5deb10f370876e83b2fa7162f54610eca44e7b73
macygabr <macygabr@student.21-school.ru>
2025-04-27 15:53:28 +0700

    fix

--- src/sections/user/view/user-view.tsx
+++ src/sections/user/view/user-view.tsx
@@ -1,5 +1,4 @@
-import { useState, useCallback } from 'react';
-import { useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Table from '@mui/material/Table';
